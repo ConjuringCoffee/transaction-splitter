80df661ee6c19242c261133bc81ebb3a18075434
ConjuringCoffee <72548231+ConjuringCoffee@users.noreply.github.com>
2022-08-28 12:10:12 +0200

    Generate test execution report

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,9 @@ module.exports = {
     'preset': 'react-native',
     'moduleFileExtensions': ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
     'setupFiles': ['./node_modules/react-native-gesture-handler/jestSetup.js'],
+    'reporters': ['default', ['jest-sonar', {
+        outputDirectory: 'reports',
+        outputName: 'test-report.xml',
+        reportedFilePath: 'absolute'
+    }]]
 };
