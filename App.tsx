30468f3d070619c1389c3dd381c1f51d8184ca74
ConjuringCoffee <72548231+ConjuringCoffee@users.noreply.github.com>
2022-09-03 14:52:46 +0200

    Apply system theme for dark / light mode (#28)

--- App.tsx
+++ App.tsx
@@ -2,11 +2,20 @@ import React from 'react';
 import * as eva from '@eva-design/eva';
 import 'react-native-gesture-handler';
 import { ApplicationProvider, IconRegistry } from '@ui-kitten/components';
-import { NavigationContainer } from '@react-navigation/native';
 import { EvaIconsPack } from '@ui-kitten/eva-icons';
-import { LogBox } from 'react-native';
+import { Appearance, LogBox, StatusBar } from 'react-native';
 import AppNavigator from './src/Helper/Navigation/AppNavigator';
-import { Provider as PaperProvider } from 'react-native-paper';
+import {
+    NavigationContainer,
+    DarkTheme as NavigationDarkTheme,
+    DefaultTheme as NavigationDefaultTheme,
+} from '@react-navigation/native';
+import {
+    DarkTheme as PaperDarkTheme,
+    DefaultTheme as PaperDefaultTheme,
+    Provider as PaperProvider,
+} from 'react-native-paper';
+import merge from 'deepmerge';
 
 LogBox.ignoreLogs([
     // Ignore this because we don't use state persistence or deep screen linking,
@@ -14,12 +23,19 @@ LogBox.ignoreLogs([
     'Non-serializable values were found in the navigation state',
 ]);
 
+const CombinedDefaultTheme = merge(PaperDefaultTheme, NavigationDefaultTheme);
+const CombinedDarkTheme = merge(PaperDarkTheme, NavigationDarkTheme);
+const colorScheme = Appearance.getColorScheme();
+
+const themeToUse = colorScheme === 'dark' ? CombinedDarkTheme : CombinedDefaultTheme;
+const evaTheme = colorScheme === 'dark' ? eva.dark : eva.light;
+
 const App = () => {
     return (
-        <PaperProvider>
+        <PaperProvider theme={themeToUse}>
             <IconRegistry icons={EvaIconsPack} />
-            <ApplicationProvider {...eva} theme={eva.light}>
-                <NavigationContainer>
+            <ApplicationProvider {...eva} theme={evaTheme}>
+                <NavigationContainer theme={themeToUse}>
                     <AppNavigator />
                 </NavigationContainer>
             </ApplicationProvider>
