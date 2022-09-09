474017258f9022a0e465f10d2b6dbd1ac2806643
ConjuringCoffee <72548231+ConjuringCoffee@users.noreply.github.com>
2022-09-09 15:00:14 +0200

    Remove unnecessary List.Sections

--- src/Screens/SettingsOverviewScreen/SettingsOverviewScreen.tsx
+++ src/Screens/SettingsOverviewScreen/SettingsOverviewScreen.tsx
@@ -3,31 +3,29 @@ import { ScreenNames } from '../../Helper/Navigation/ScreenNames';
 import { List } from 'react-native-paper';
 import { View } from 'react-native';
 
-type ScreenName = 'Settings Overview';;
+type ScreenName = 'Settings Overview';
 
 export const SettingsOverviewScreen = ({ navigation }: MyStackScreenProps<ScreenName>) => {
     return (
         <View>
-            <List.Section>
-                <List.Item
-                    title='Access Token'
-                    description='Necessary to access the YNAB API'
-                    onPress={() => {
-                        navigation.navigate(ScreenNames.ACCESS_TOKEN_SCREEN);
-                    }} />
-                <List.Item
-                    title='Profiles'
-                    description='The two profiles to split the transactions to'
-                    onPress={() => {
-                        navigation.navigate(ScreenNames.PROFILE_SETTINGS_SCREEN);
-                    }} />
-                <List.Item
-                    title='Category Combinations'
-                    description='Combine categories of your profiles'
-                    onPress={() => {
-                        navigation.navigate(ScreenNames.CATEGORY_COMBO_SETTINGS_SCREEN);
-                    }} />
-            </List.Section>
+            <List.Item
+                title='Access Token'
+                description='Necessary to access the YNAB API'
+                onPress={() => {
+                    navigation.navigate(ScreenNames.ACCESS_TOKEN_SCREEN);
+                }} />
+            <List.Item
+                title='Profiles'
+                description='The two profiles to split the transactions to'
+                onPress={() => {
+                    navigation.navigate(ScreenNames.PROFILE_SETTINGS_SCREEN);
+                }} />
+            <List.Item
+                title='Category Combinations'
+                description='Combine categories of your profiles'
+                onPress={() => {
+                    navigation.navigate(ScreenNames.CATEGORY_COMBO_SETTINGS_SCREEN);
+                }} />
         </View>
     );
 };
