22b7f2f96e53ed1ba20921b0e5c11bbf2ac4587e
ConjuringCoffee <72548231+ConjuringCoffee@users.noreply.github.com>
2022-09-10 13:10:35 +0200

    Migrate to r-n-paper on AccessTokenScreen

--- src/Screens/AccessTokenScreen/AccessTokenScreen.tsx
+++ src/Screens/AccessTokenScreen/AccessTokenScreen.tsx
@@ -1,28 +1,61 @@
-import { Button, Input } from '@ui-kitten/components';
-import React, { useState } from 'react';
+import React, { useLayoutEffect, useState } from 'react';
 import { View } from 'react-native';
+import { Appbar, TextInput } from 'react-native-paper';
+import { NavigationBar } from '../../Helper/Navigation/NavigationBar';
+import { MyStackScreenProps } from '../../Helper/Navigation/ScreenParameters';
 import { saveAccessToken, selectAccessToken } from '../../redux/features/accessToken/accessTokenSlice';
 import { useAppDispatch, useAppSelector } from '../../redux/hooks';
 
-export const AccessTokenScreen = () => {
+type ScreenName = 'Access Token';
+
+const SCREEN_TITLE = 'Access Token';
+
+const ICON_SAVE = 'content-save';
+const ICON_INPUT_HIDDEN = 'eye-off';
+const ICON_INPUT_VISIBLE = 'eye';
+
+export const AccessTokenScreen = ({ navigation }: MyStackScreenProps<ScreenName>) => {
     const dispatch = useAppDispatch();
     const accessToken = useAppSelector(selectAccessToken);
 
     const [enteredToken, setEnteredToken] = useState<string>('');
+    const [inputHidden, setInputHidden] = useState<boolean>(true);
+
+
+    useLayoutEffect(() => {
+        const addition = (
+            <Appbar.Action
+                icon={ICON_SAVE}
+                onPress={() => {
+                    dispatch(saveAccessToken(enteredToken));
+                }} />);
+
+        navigation.setOptions({
+            header: () => (
+                <NavigationBar
+                    title={SCREEN_TITLE}
+                    navigation={navigation}
+                    additions={addition}
+                />
+            ),
+        });
+    }, [navigation, dispatch, enteredToken]);
 
     return (
         <View>
-            <Input
-                placeholder="YNAB Personal Access Token"
+            <TextInput
+                label="YNAB Personal Access Token"
                 onChangeText={setEnteredToken}
                 defaultValue={accessToken}
+                secureTextEntry={inputHidden}
+                right={
+                    <TextInput.Icon
+                        icon={inputHidden ? ICON_INPUT_HIDDEN : ICON_INPUT_VISIBLE}
+                        onPress={() => setInputHidden(!inputHidden)}
+                        forceTextInputFocus={false}
+                    />
+                }
             />
-            <Button
-                onPress={() => {
-                    dispatch(saveAccessToken(enteredToken));
-                }}>
-                Set keychain
-            </Button>
         </View>
     );
 };
