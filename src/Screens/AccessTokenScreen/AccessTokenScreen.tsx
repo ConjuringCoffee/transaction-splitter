3cef95efde013e7254f81f3904d4cdac8ceb0bff
ConjuringCoffee <72548231+ConjuringCoffee@users.noreply.github.com>
2022-09-10 14:35:37 +0200

    Add connection test to AccessTokenScreen

--- src/Screens/AccessTokenScreen/AccessTokenScreen.tsx
+++ src/Screens/AccessTokenScreen/AccessTokenScreen.tsx
@@ -1,11 +1,13 @@
 import { useLayoutEffect, useState } from 'react';
-import { View } from 'react-native';
-import { Appbar, TextInput } from 'react-native-paper';
+import { View, StyleSheet } from 'react-native';
+import { Appbar, Button, HelperText, TextInput } from 'react-native-paper';
 import { useNavigateBack } from '../../Helper/Navigation/navigateBack';
 import { NavigationBar } from '../../Helper/Navigation/NavigationBar';
 import { MyStackScreenProps } from '../../Helper/Navigation/ScreenParameters';
 import { saveAccessToken, selectAccessToken } from '../../redux/features/accessToken/accessTokenSlice';
 import { useAppDispatch, useAppSelector } from '../../redux/hooks';
+import { API } from 'ynab';
+import { LoadingStatus } from '../../Helper/LoadingStatus';
 
 type ScreenName = 'Access Token';
 
@@ -14,6 +16,16 @@ const SCREEN_TITLE = 'Access Token';
 const ICON_SAVE = 'content-save';
 const ICON_INPUT_HIDDEN = 'eye-off';
 const ICON_INPUT_VISIBLE = 'eye';
+const ICON_CONNECTION_SUCCESS = 'check';
+const ICON_CONNECTION_ERROR = 'alert-circle';
+
+interface ConnectionStatus {
+    status: LoadingStatus,
+    error?: {
+        id: string,
+        detail: string
+    }
+}
 
 export const AccessTokenScreen = ({ navigation }: MyStackScreenProps<ScreenName>) => {
     const dispatch = useAppDispatch();
@@ -21,7 +33,25 @@ export const AccessTokenScreen = ({ navigation }: MyStackScreenProps<ScreenName>
 
     const [enteredToken, setEnteredToken] = useState<string>(accessToken);
     const [inputHidden, setInputHidden] = useState<boolean>(true);
+    const [connectionStatus, setConnectionStatus] = useState<ConnectionStatus>({ status: LoadingStatus.IDLE });
     const [navigateBack] = useNavigateBack(navigation);
+
+    const testConnection = async () => {
+        setConnectionStatus({ status: LoadingStatus.LOADING });
+
+        try {
+            await new API(enteredToken).user.getUser();
+            setConnectionStatus({ status: LoadingStatus.SUCCESSFUL });
+        } catch (error: any) {
+            setConnectionStatus({
+                status: LoadingStatus.ERROR,
+                error: {
+                    id: error.error.id,
+                    detail: error.error.detail,
+                },
+            });
+        }
+    };
 
     useLayoutEffect(() => {
         const addition = (
@@ -47,9 +77,13 @@ export const AccessTokenScreen = ({ navigation }: MyStackScreenProps<ScreenName>
         <View>
             <TextInput
                 label="YNAB Personal Access Token"
-                onChangeText={setEnteredToken}
+                onChangeText={(text) => {
+                    setEnteredToken(text);
+                }}
                 value={enteredToken}
                 secureTextEntry={inputHidden}
+                error={connectionStatus.status === LoadingStatus.ERROR}
+                style={styles.input}
                 right={
                     <TextInput.Icon
                         icon={inputHidden ? ICON_INPUT_HIDDEN : ICON_INPUT_VISIBLE}
@@ -58,6 +92,29 @@ export const AccessTokenScreen = ({ navigation }: MyStackScreenProps<ScreenName>
                     />
                 }
             />
-        </View>
+            {connectionStatus.status === LoadingStatus.ERROR
+                ? <HelperText type="error">
+                    {`${connectionStatus.error?.id}: ${connectionStatus.error?.detail}`}
+                </HelperText>
+                : null
+            }
+            <Button
+                loading={connectionStatus.status === LoadingStatus.LOADING}
+                icon={connectionStatus.status === LoadingStatus.SUCCESSFUL
+                    ? ICON_CONNECTION_SUCCESS
+                    : connectionStatus.status === LoadingStatus.ERROR
+                        ? ICON_CONNECTION_ERROR
+                        : undefined}
+                onPress={testConnection} >
+                Test connection
+            </Button>
+
+        </View >
     );
 };
+
+const styles = StyleSheet.create({
+    input: {
+        margin: 8,
+    },
+});
