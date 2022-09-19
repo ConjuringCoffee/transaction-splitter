76ad1cc05e51bc29a6c0fbaa2fd686b74e22b479
ConjuringCoffee <72548231+ConjuringCoffee@users.noreply.github.com>
2022-09-19 23:29:08 +0200

    Use FAB as the save button

--- src/Screens/SaveScreen/SaveScreen.tsx
+++ src/Screens/SaveScreen/SaveScreen.tsx
@@ -1,11 +1,7 @@
 import React, { useEffect, useState } from 'react';
-import { RouteProp } from '@react-navigation/native';
-import { Button } from '@ui-kitten/components';
-import { StyleSheet } from 'react-native';
 import { createTransaction } from '../../YnabApi/YnabApiWrapper';
 import { ScrollView } from 'react-native-gesture-handler';
-import { StackNavigationProp } from '@react-navigation/stack';
-import { StackParameterList } from '../../Helper/Navigation/ScreenParameters';
+import { MyStackScreenProps } from '../../Helper/Navigation/ScreenParameters';
 import { useLocalization } from '../../Hooks/useLocalization';
 import { LoadingComponent } from '../../Component/LoadingComponent';
 import { ScreenNames } from '../../Helper/Navigation/ScreenNames';
@@ -13,35 +9,70 @@ import { useAppSelector } from '../../redux/hooks';
 import { selectAccessToken } from '../../redux/features/accessToken/accessTokenSlice';
 import { LoadingStatus } from '../../Helper/LoadingStatus';
 import { SaveTransactionListSection } from './SaveTransactionListSection';
+import { SaveFAB } from './SaveFAB';
 
-type MyNavigationProp = StackNavigationProp<StackParameterList, 'Save'>;
-type MyRouteProp = RouteProp<StackParameterList, 'Save'>;
+type ScreenName = 'Save';
 
-type Props = {
-    navigation: MyNavigationProp;
-    route: MyRouteProp;
-}
-
-export const SaveScreen = ({ navigation, route }: Props) => {
+export const SaveScreen = ({ navigation, route }: MyStackScreenProps<ScreenName>) => {
     const { numberFormatSettings } = useLocalization();
 
     const [payerTransactionSaveStatus, setPayerTransactionSaveStatus] = useState<LoadingStatus>(LoadingStatus.IDLE);
     const [debtorTransactionSaveStatus, setDebtorTransactionSaveStatus] = useState<LoadingStatus>(LoadingStatus.IDLE);
-
-    const { basicData } = route.params;
-    const { payerSaveTransaction } = route.params;
-    const { debtorSaveTransaction } = route.params;
-
     const accessToken = useAppSelector(selectAccessToken);
 
+    const {
+        basicData,
+        payerSaveTransaction,
+        debtorSaveTransaction,
+    } = route.params;
+
     useEffect(() => {
-        if (payerTransactionSaveStatus === LoadingStatus.SUCCESSFUL && debtorTransactionSaveStatus === LoadingStatus.SUCCESSFUL) {
+        const navigate = async () => {
             navigation.reset({
                 index: 0,
                 routes: [{ name: ScreenNames.SPLITTING_SCREEN }],
             });
+        };
+
+        if (payerTransactionSaveStatus === LoadingStatus.SUCCESSFUL && debtorTransactionSaveStatus === LoadingStatus.SUCCESSFUL) {
+            setTimeout(navigate, 500);
         }
     }, [navigation, payerTransactionSaveStatus, debtorTransactionSaveStatus]);
+
+    const save = () => {
+        setPayerTransactionSaveStatus(LoadingStatus.LOADING);
+        setDebtorTransactionSaveStatus(LoadingStatus.LOADING);
+
+        createTransaction(basicData.payer.budgetId, payerSaveTransaction, accessToken)
+            .then(() => {
+                setPayerTransactionSaveStatus(LoadingStatus.SUCCESSFUL);
+            }).catch((error) => {
+                console.error(error);
+                setPayerTransactionSaveStatus(LoadingStatus.ERROR);
+            });
+
+        createTransaction(basicData.debtor.budgetId, debtorSaveTransaction, accessToken)
+            .then(() => {
+                setDebtorTransactionSaveStatus(LoadingStatus.SUCCESSFUL);
+            }).catch((error) => {
+                console.error(error);
+                setDebtorTransactionSaveStatus(LoadingStatus.ERROR);
+            });
+    };
+
+    const getOverallSaveStatus = (): LoadingStatus => {
+        if (payerTransactionSaveStatus === LoadingStatus.ERROR || debtorTransactionSaveStatus === LoadingStatus.ERROR) {
+            return LoadingStatus.ERROR;
+        } else if (payerTransactionSaveStatus === LoadingStatus.SUCCESSFUL && debtorTransactionSaveStatus === LoadingStatus.SUCCESSFUL) {
+            return LoadingStatus.SUCCESSFUL;
+        } else if (payerTransactionSaveStatus === LoadingStatus.IDLE && debtorTransactionSaveStatus === LoadingStatus.IDLE) {
+            return LoadingStatus.IDLE;
+        } else {
+            return LoadingStatus.LOADING;
+        }
+    };
+
+    const overallSaveStatus = getOverallSaveStatus();
 
     return (
         <>
@@ -66,38 +97,12 @@ export const SaveScreen = ({ navigation, route }: Props) => {
                             numberFormatSettings={numberFormatSettings}
                         />
                     </ScrollView>
-                    <Button
-                        style={styles.button}
-                        onPress={() => {
-                            setPayerTransactionSaveStatus(LoadingStatus.LOADING);
-                            setDebtorTransactionSaveStatus(LoadingStatus.LOADING);
-
-                            createTransaction(basicData.payer.budgetId, payerSaveTransaction, accessToken)
-                                .then(() => {
-                                    setPayerTransactionSaveStatus(LoadingStatus.SUCCESSFUL);
-                                }).catch((error) => {
-                                    console.error(error);
-                                    setPayerTransactionSaveStatus(LoadingStatus.ERROR);
-                                });
-
-                            createTransaction(basicData.debtor.budgetId, debtorSaveTransaction, accessToken)
-                                .then(() => {
-                                    setDebtorTransactionSaveStatus(LoadingStatus.SUCCESSFUL);
-                                }).catch((error) => {
-                                    console.error(error);
-                                    setDebtorTransactionSaveStatus(LoadingStatus.ERROR);
-                                });
-                        }}>
-                        Save transactions
-                    </Button>
+                    <SaveFAB
+                        saveStatus={overallSaveStatus}
+                        save={save}
+                    />
                 </>
                 : <LoadingComponent />}
         </>
     );
 };
-
-const styles = StyleSheet.create({
-    button: {
-        margin: 6,
-    },
-});
