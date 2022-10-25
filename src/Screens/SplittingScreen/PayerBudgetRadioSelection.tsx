c45215fcf706dcee809de04c1778de7c17700ca7
ConjuringCoffee <72548231+ConjuringCoffee@users.noreply.github.com>
2022-10-25 10:25:49 +0200

    Dismiss keyboard when budget is selected

--- src/Screens/SplittingScreen/PayerBudgetRadioSelection.tsx
+++ src/Screens/SplittingScreen/PayerBudgetRadioSelection.tsx
@@ -1,6 +1,7 @@
 import { List, RadioButton } from 'react-native-paper';
 import { BudgetInProfile, Profile } from '../../redux/features/profiles/profilesSlice';
-import React from 'react';
+import React, { useCallback } from 'react';
+import { Keyboard } from 'react-native';
 
 interface Props {
     profile: Profile,
@@ -9,17 +10,26 @@ interface Props {
 }
 
 export const PayerBudgetRadioSelection = (props: Props) => {
+    const { setPayerBudgetIndex } = props;
+
+    const onValueChange = useCallback((newValue: string) => {
+        Keyboard.dismiss();
+        setPayerBudgetIndex(Number(newValue));
+    }, [setPayerBudgetIndex]);
+
     const renderItem = (budgetInProfile: BudgetInProfile, index: number) => (
         <RadioButton.Item
             key={budgetInProfile.budgetId}
             // TODO: Use original budget name if empty
             label={budgetInProfile.name ?? '???'}
-            value={index.toString()} />
+            value={index.toString()}
+        />
     );
+
     return (
         <List.Section title='Payer budget'>
             <RadioButton.Group
-                onValueChange={(newValue) => props.setPayerBudgetIndex(Number(newValue))}
+                onValueChange={onValueChange}
                 value={props.payerBudgetIndex.toString()}
             >
                 {props.profile.budgets.map(renderItem)}
