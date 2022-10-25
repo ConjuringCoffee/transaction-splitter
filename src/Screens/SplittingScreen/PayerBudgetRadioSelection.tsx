7478ebc07f0946b98d736a36cbfcbf53dc26c59f
ConjuringCoffee <72548231+ConjuringCoffee@users.noreply.github.com>
2022-10-25 10:33:46 +0200

    Display budget name if no display name is set

--- src/Screens/SplittingScreen/PayerBudgetRadioSelection.tsx
+++ src/Screens/SplittingScreen/PayerBudgetRadioSelection.tsx
@@ -2,6 +2,8 @@ import { List, RadioButton } from 'react-native-paper';
 import { BudgetInProfile, Profile } from '../../redux/features/profiles/profilesSlice';
 import React, { useCallback } from 'react';
 import { Keyboard } from 'react-native';
+import { useAppSelector } from '../../redux/hooks';
+import { selectBudgets } from '../../redux/features/ynab/ynabSlice';
 
 interface Props {
     profile: Profile,
@@ -9,30 +11,35 @@ interface Props {
     setPayerBudgetIndex: (index: number) => void,
 }
 
-export const PayerBudgetRadioSelection = (props: Props) => {
-    const { setPayerBudgetIndex } = props;
+export const PayerBudgetRadioSelection = ({ profile, payerBudgetIndex, setPayerBudgetIndex }: Props) => {
+    const budgets = useAppSelector(selectBudgets);
 
     const onValueChange = useCallback((newValue: string) => {
         Keyboard.dismiss();
         setPayerBudgetIndex(Number(newValue));
     }, [setPayerBudgetIndex]);
 
-    const renderItem = (budgetInProfile: BudgetInProfile, index: number) => (
-        <RadioButton.Item
+    const renderItem = useCallback((budgetInProfile: BudgetInProfile, index: number) => {
+        const displayedName = budgetInProfile.name ?? budgets.find((budget) => budget.id === budgetInProfile.budgetId)?.name;
+
+        if (displayedName === undefined) {
+            throw new Error(`Budget for ID ${budgetInProfile.budgetId} was not found`);
+        }
+
+        return (<RadioButton.Item
             key={budgetInProfile.budgetId}
-            // TODO: Use original budget name if empty
-            label={budgetInProfile.name ?? '???'}
+            label={displayedName}
             value={index.toString()}
-        />
-    );
+        />);
+    }, [budgets]);
 
     return (
         <List.Section title='Payer budget'>
             <RadioButton.Group
                 onValueChange={onValueChange}
-                value={props.payerBudgetIndex.toString()}
+                value={payerBudgetIndex.toString()}
             >
-                {props.profile.budgets.map(renderItem)}
+                {profile.budgets.map(renderItem)}
             </RadioButton.Group>
         </List.Section>
     );
