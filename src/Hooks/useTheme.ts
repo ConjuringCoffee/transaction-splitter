d336ae1a10950f2e59c5ee2ab3d87c83daa7e240
ConjuringCoffee <72548231+ConjuringCoffee@users.noreply.github.com>
2026-05-25 15:46:10 +0200

    Change background color

--- src/Hooks/useTheme.ts
+++ src/Hooks/useTheme.ts
@@ -21,6 +21,7 @@ export const useTheme = () => {
         theme.colors.onPrimary = '#FFFFFF';
         theme.colors.secondaryContainer = '#B2DEE3';
         theme.colors.onSecondaryContainer = '#003640';
+        theme.colors.background = '#F5FAFB';
         theme.colors.elevation = { ...theme.colors.elevation, level1: '#EDF5F6' };
         return theme;
     }, [styles]);
@@ -31,6 +32,7 @@ export const useTheme = () => {
         theme.colors.onPrimary = '#FFFFFF';
         theme.colors.secondaryContainer = '#1E5560';
         theme.colors.onSecondaryContainer = '#B2DEE3';
+        theme.colors.background = '#141C1E';
         theme.colors.elevation = { ...theme.colors.elevation, level1: '#1A2A2F' };
         return theme;
     }, [styles]);
