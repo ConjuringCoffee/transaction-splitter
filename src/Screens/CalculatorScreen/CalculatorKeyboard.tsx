5923c8637633f1681696d872db774a14c54db09a
ConjuringCoffee <72548231+ConjuringCoffee@users.noreply.github.com>
2026-05-25 12:33:42 +0200

    Add dark mode to calculator keyboard (#176)

--- src/Screens/CalculatorScreen/CalculatorKeyboard.tsx
+++ src/Screens/CalculatorScreen/CalculatorKeyboard.tsx
@@ -17,8 +17,6 @@ type Props = {
     onCancelPress: () => void
 }
 
-const DARK_COLOR = '#e6e5ea';
-
 export const CalculatorKeyboard = (props: Props) => {
     const numberFormatSettings = useAppSelector(selectNumberFormatSettings);
 
@@ -28,96 +26,49 @@ export const CalculatorKeyboard = (props: Props) => {
                 <View style={styles.numberRow}>
                     <KeyboardButton
                         value='AC'
-                        color={DARK_COLOR}
+                        variant='action'
                         onPress={props.onClearPress}
                     />
                     <KeyboardButton
                         value='Del'
-                        color={DARK_COLOR}
+                        variant='action'
                         onPress={props.onBackPress}
                     />
                 </View>
                 <View style={styles.numberRow}>
-                    <KeyboardNumberButton
-                        number={7}
-                        onPress={props.onDigitPress}
-                    />
-                    <KeyboardNumberButton
-                        number={8}
-                        onPress={props.onDigitPress}
-                    />
-                    <KeyboardNumberButton
-                        number={9}
-                        onPress={props.onDigitPress}
-                    />
+                    <KeyboardNumberButton number={7} onPress={props.onDigitPress} />
+                    <KeyboardNumberButton number={8} onPress={props.onDigitPress} />
+                    <KeyboardNumberButton number={9} onPress={props.onDigitPress} />
                 </View>
                 <View style={styles.numberRow}>
-                    <KeyboardNumberButton
-                        number={4}
-                        onPress={props.onDigitPress}
-                    />
-                    <KeyboardNumberButton
-                        number={5}
-                        onPress={props.onDigitPress}
-                    />
-                    <KeyboardNumberButton
-                        number={6}
-                        onPress={props.onDigitPress}
-                    />
+                    <KeyboardNumberButton number={4} onPress={props.onDigitPress} />
+                    <KeyboardNumberButton number={5} onPress={props.onDigitPress} />
+                    <KeyboardNumberButton number={6} onPress={props.onDigitPress} />
                 </View>
                 <View style={styles.numberRow}>
-                    <KeyboardNumberButton
-                        number={3}
-                        onPress={props.onDigitPress}
-                    />
-                    <KeyboardNumberButton
-                        number={2}
-                        onPress={props.onDigitPress}
-                    />
-                    <KeyboardNumberButton
-                        number={1}
-                        onPress={props.onDigitPress}
-                    />
+                    <KeyboardNumberButton number={3} onPress={props.onDigitPress} />
+                    <KeyboardNumberButton number={2} onPress={props.onDigitPress} />
+                    <KeyboardNumberButton number={1} onPress={props.onDigitPress} />
                 </View>
                 <View style={styles.numberRow}>
                     <KeyboardButton
                         value='Cancel'
-                        color={DARK_COLOR}
+                        variant='action'
                         onPress={props.onCancelPress}
                     />
-                    <KeyboardNumberButton
-                        number={0}
-                        onPress={props.onDigitPress}
-                    />
+                    <KeyboardNumberButton number={0} onPress={props.onDigitPress} />
                     <KeyboardButton
                         value={numberFormatSettings.decimalSeparator}
-                        color='white'
+                        variant='digit'
                         onPress={props.onDecimalSeparatorPress}
                     />
-
                 </View>
             </View>
             <View style={styles.operatorLayout}>
-                <KeyboardButton
-                    value='-'
-                    color={DARK_COLOR}
-                    onPress={props.onOperatorSubtractPress}
-                />
-                <KeyboardButton
-                    value='+'
-                    color={DARK_COLOR}
-                    onPress={props.onOperatorAddPress}
-                />
-                <KeyboardButton
-                    value='='
-                    color={DARK_COLOR}
-                    onPress={props.onCalculatePress}
-                />
-                <KeyboardButton
-                    value='OK'
-                    color={DARK_COLOR}
-                    onPress={props.onConfirmPress}
-                />
+                <KeyboardButton value='-' variant='action' onPress={props.onOperatorSubtractPress} />
+                <KeyboardButton value='+' variant='action' onPress={props.onOperatorAddPress} />
+                <KeyboardButton value='=' variant='action' onPress={props.onCalculatePress} />
+                <KeyboardButton value='OK' variant='action' onPress={props.onConfirmPress} />
             </View>
         </View>
     );
