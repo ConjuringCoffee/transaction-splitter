3ab14c3ba47189c930b75409a017829c3fd8e3c1
ConjuringCoffee <72548231+ConjuringCoffee@users.noreply.github.com>
2026-05-26 18:06:43 +0200

    Add surface around list items

--- src/Screens/Settings/CategoryCombos/CategoryComboSettingsScreen/CategoryComboSettingsScreen.tsx
+++ src/Screens/Settings/CategoryCombos/CategoryComboSettingsScreen/CategoryComboSettingsScreen.tsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useMemo } from 'react';
 import { MyStackScreenProps } from '../../../../Navigation/ScreenParameters';
 import { ScreenNames } from '../../../../Navigation/ScreenNames';
-import { Appbar, List } from 'react-native-paper';
+import { Appbar, List, Surface } from 'react-native-paper';
 import { CategoryCombo, selectCategoryCombos } from '../../../../redux/features/categoryCombos/categoryCombosSlice';
 import { useAppSelector } from '../../../../Hooks/useAppSelector';
 import { View } from 'react-native';
+import { CustomScrollView } from '../../../../Component/CustomScrollView';
 import { selectProfile } from '../../../../redux/features/profile/profileSlice';
 import { fetchCategoryGroups, selectCategoriesFetchStatus } from '../../../../redux/features/ynab/ynabSlice';
 import { LoadingStatus } from '../../../../Helper/LoadingStatus';
@@ -74,10 +75,18 @@ export const CategoryComboSettingsScreen = ({ navigation }: MyStackScreenProps<S
         additions: navigationBarAddition,
     });
 
+    const cardStyle = {
+        borderRadius: theme.roundness * 3,
+        overflow: 'hidden' as const,
+    };
+
+    const chevronRight = (iconProps: object) => <List.Icon {...iconProps} icon='chevron-right' />;
+
     const renderListItem = (categoryCombo: CategoryCombo) => (
         <List.Item
             key={categoryCombo.id}
             title={categoryCombo.name}
+            right={chevronRight}
             onPress={() => {
                 navigation.navigate(ScreenNames.EDIT_CATEGORY_COMBO_SCREEN, {
                     categoryCombo: categoryCombo,
@@ -87,12 +96,16 @@ export const CategoryComboSettingsScreen = ({ navigation }: MyStackScreenProps<S
     );
 
     return (
-        <View>
-            {
-                everythingLoaded
-                    ? sortedCategoryCombos.map(renderListItem)
-                    : <LoadingComponent />
-            }
+        <View style={{ flex: 1 }}>
+            {everythingLoaded
+                ? (
+                    <CustomScrollView>
+                        <Surface elevation={1} style={cardStyle}>
+                            {sortedCategoryCombos.map(renderListItem)}
+                        </Surface>
+                    </CustomScrollView>
+                )
+                : <LoadingComponent />}
         </View>
     );
 };
