5ee1d9f24e1091b1897588e0e2f70541fac8d5ac
ConjuringCoffee <72548231+ConjuringCoffee@users.noreply.github.com>
2026-05-26 18:24:38 +0200

    Redesign CategoryComboInputView

--- src/Screens/Settings/CategoryCombos/CategoryComboInputView.tsx
+++ src/Screens/Settings/CategoryCombos/CategoryComboInputView.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import { View, StyleSheet } from 'react-native';
-import { List, TextInput } from 'react-native-paper';
+import { View } from 'react-native';
+import { Surface, TextInput } from 'react-native-paper';
 import { MyStackNavigationProp, StackParameterList } from '../../../Navigation/ScreenParameters';
 import { selectProfile } from '../../../redux/features/profile/profileSlice';
 import { useAppSelector } from '../../../Hooks/useAppSelector';
 import { ChooseCategoryListItem } from './ChooseCategoryListItem';
+import { CustomScrollView } from '../../../Component/CustomScrollView';
+import { useTheme } from '../../../Hooks/useTheme';
 
 type Props<T extends keyof StackParameterList> = {
     navigation: MyStackNavigationProp<T>,
@@ -18,40 +20,39 @@ type Props<T extends keyof StackParameterList> = {
 
 export const CategoryComboInputView = <T extends keyof StackParameterList>(props: Props<T>) => {
     const profile = useAppSelector(selectProfile);
+    const [theme] = useTheme();
+
+    const cardStyle = {
+        borderRadius: theme.roundness * 3,
+        overflow: 'hidden' as const,
+    };
 
     return (
-        <View>
-            <TextInput
-                value={props.name}
-                onChangeText={props.setName}
-                style={styles.input}
-                label='Category Combination Name'
-            />
-            <List.Section>
-                <List.Subheader>
-                    Categories
-                </List.Subheader>
-                <ChooseCategoryListItem
-                    budgetId={profile!.budgets[0].budgetId}
-                    budgetDisplayName={profile!.budgets[0].name}
-                    navigation={props.navigation}
-                    selectedCategoryId={props.categoryIdFirstProfile}
-                    onCategorySelect={props.setCategoryIdFirstProfile}
+        <CustomScrollView>
+            <View style={{ gap: theme.spacing }}>
+                <TextInput
+                    value={props.name}
+                    onChangeText={props.setName}
+                    label='Category Combination Name'
+                    mode='outlined'
                 />
-                <ChooseCategoryListItem
-                    budgetId={profile!.budgets[1].budgetId}
-                    budgetDisplayName={profile!.budgets[1].name}
-                    navigation={props.navigation}
-                    selectedCategoryId={props.categoryIdSecondProfile}
-                    onCategorySelect={props.setCategoryIdSecondProfile}
-                />
-            </List.Section>
-        </View>
+                <Surface elevation={1} style={cardStyle}>
+                    <ChooseCategoryListItem
+                        budgetId={profile!.budgets[0].budgetId}
+                        budgetDisplayName={profile!.budgets[0].name}
+                        navigation={props.navigation}
+                        selectedCategoryId={props.categoryIdFirstProfile}
+                        onCategorySelect={props.setCategoryIdFirstProfile}
+                    />
+                    <ChooseCategoryListItem
+                        budgetId={profile!.budgets[1].budgetId}
+                        budgetDisplayName={profile!.budgets[1].name}
+                        navigation={props.navigation}
+                        selectedCategoryId={props.categoryIdSecondProfile}
+                        onCategorySelect={props.setCategoryIdSecondProfile}
+                    />
+                </Surface>
+            </View>
+        </CustomScrollView>
     );
 };
-
-const styles = StyleSheet.create({
-    input: {
-        margin: 8,
-    },
-});
