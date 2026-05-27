ef3e801b9147431c23977a21220008f433d7619c
ConjuringCoffee <72548231+ConjuringCoffee@users.noreply.github.com>
2026-05-27 19:29:36 +0200

    Transfer test coverage to Sonar

--- jest.config.cjs
+++ jest.config.cjs
@@ -2,4 +2,6 @@ module.exports = {
     'preset': 'react-native',
     'moduleFileExtensions': ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
     'setupFiles': ['./node_modules/react-native-gesture-handler/jestSetup.js'],
+    'coverageReporters': ['lcov', 'text'],
+    'coverageDirectory': 'coverage',
 };
